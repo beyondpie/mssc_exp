db046e56cd3d392c61948aa1e1ef1573f0316d28
beyondpie <zsp07@tsinghua.org.cn>
2020-04-30 22:24:20 -0400

    for test.

--- src/screen.sh
+++ src/screen.sh
@@ -2,12 +2,11 @@
 
 for i in 2 3 4 5
 do
+    model="model_i_${i}"
+    make MODEL=${model} compile
     for g in HBB LYZ GPX1
     do
-        model="model_i_${i}"
-        echo "Model: ${model} For GENE: ${g}"
-        make MODEL=${model} GENE=${gene} compile
-        make MODEL=${model} GENE=${gene} ssample
+        make MODEL=${model} GENE=${g} ssample
     done
 done
 
