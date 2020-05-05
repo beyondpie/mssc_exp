eae5c30093d6f5efe1e2e99dfdc147031d902f52
beyondpie <zsp07@tsinghua.org.cn>
2020-05-05 14:54:38 -0400

    compare different variances.

--- model_ii/rstan_analyzer.R
+++ model_ii/rstan_analyzer.R
@@ -1,37 +1,68 @@
 import::from(
   rstan, stan, extract, read_rdump, read_stan_csv,
   check_hmc_diagnostics, sampling, stan_rdump, plot,
-  get_sampler_params,traceplot
+  get_sampler_params, traceplot
 )
-model <- "model_ii_1"
 
-# model 2, 4 not that good.
-gene <- "HBB"
-gene  <- "LYZ"
-model_fnm <- paste0("./", model, "_", gene, ".csv")
-fit <- read_stan_csv(model_fnm)
+draw_invgamma <- function(alpha, rate = 1 / alpha, from = 0.0001, to = 20) {
+  library(invgamma)
+  library(ggplot2)
+  theme_set(theme_bw())
+  x <- seq(from, to, 0.01)
+  qplot(x, dinvgamma(x, alpha, rate), geom = "line")
+}
 
-sampler_params <- get_sampler_params(fit, inc_warmup = TRUE)
-summary(do.call(rbind, sampler_params), digits = 2)
+plot_posterior <- function(modelnm, genenm, param = c("mu_g_ic", "mu", "mu_g_di")) {
+  library(ggplot2)
+  library(ggpubr)
 
-param <- "mu_g_ic"
-param <- "mu_g_di"
+  model_fnm <- paste0("./", modelnm, "_", genenm, ".csv")
+  fit <- read_stan_csv(model_fnm)
+  p1 <- plot(fit, pars = c(param))
 
-## par(mfrow=c(2,2))
-plot(fit, pars = c(param))
-plot(fit, pars = c(param), plotfun = "hist")
-traceplot(fit, pars=c(param), inc_warmup=TRUE, nrow=2)
-## pairs(fit, pars = c(param), las = 1)
+  dfr <- as.data.frame(fit)
+  diff_di <- data.frame(diff = dfr[["mu_g_di[1]"]] - dfr[["mu_g_di[2]"]])
 
-dfr <- as.data.frame(fit)
+  hist <- ggplot(diff_di, aes(x = diff)) +
+    geom_histogram(color = "blue", fill = "blue")
 
-diff  <- dfr[["mu_g_di[1]"]]  - dfr[["mu_g_di[2]"]]
-hist(diff, breaks=100)
-quantile(diff, 0.025)
-quantile(diff, 0.9725)
-ecdf(diff)(0.0)
+  p <- ggarrange(p1, hist, ncol = 1, nrow = 2)
+  print(p)
+
+  print(quantile(diff, 0.025))
+  print(quantile(diff, 0.975))
+}
+
+plot_posterior("model_ii_1", "HBB")
+plot_posterior("model_ii_2", "HBB")
+plot_posterior("model_ii_3", "HBB")
+
+plot_posterior("model_ii_1", "LYZ")
+plot_posterior("model_ii_2", "LYZ")
+plot_posterior("model_ii_3", "LYZ")
+
+## TEST
+## model <- "model_ii_1"
+## model <- "model_ii_2"
+## model <- "model_ii_3"
+
+## # model 2, 4 not that good. gene <- "HBB"
+## gene <- "HBA2"
+## gene <- "LYZ"
+## model_fnm <- paste0("./", model, "_", gene, ".csv")
+## fit <- read_stan_csv(model_fnm)
+## sampler_params <- get_sampler_params(fit, inc_warmup = TRUE)
+## summary(do.call(rbind, sampler_params), digits = 2)
+## param <- "mu_g_ic"
+## param <- "mu_g_di"
+## param <- "mu"
+## param <- c("mu_g_ic", "mu", "mu_g_di")
+## ## par(mfrow=c(2,2))
+## plot(fit, pars = c(param))
+## plot(fit, pars = c(param), plotfun = "hist")
+## traceplot(fit, pars = c(param), inc_warmup = TRUE, nrow = 2)
+## ## ecdf(diff)(0.0)
 
 # foldchange, influenced by small values quite a lot.
 # fc <- exp(diff)
 # hist(fc, breaks=100)
-
--- model_ii/screen.sh
+++ model_ii/screen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in 2 3 4 5
+for i in 3
 do
-    model="model_i_${i}"
+    model="model_ii_${i}"
     make MODEL=${model} compile
-    for g in HBB LYZ GPX1
+    for g in HBA2 HBA1 CXCL8 LYZ GPX1 CST3
     do
         make MODEL=${model} GENE=${g} ssample >${model}_${g}_sample.log &
     done
