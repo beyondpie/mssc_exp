f763361d37756ddf9e5de2dc1d0c27fcd921a695
beyondpie <zsp07@tsinghua.org.cn>
2020-05-14 23:33:51 -0400

    correct mc

--- model_v4/mc.sh
+++ model_v4/mc.sh
@@ -4,7 +4,8 @@
 
 for i in {1..4}
 do
-    ./${1} sample random seed=355113 num_samples=1000 num_warmup=1000\
+    ./${1} method=sample num_samples=1000 num_warmup=1000\
+         adapt random seed=355113 \
          id=${i} data file=${2} \
          output refresh=5 file=${1}${i}.csv >${1}${i}.log &
 done
