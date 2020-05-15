e5b4b709af42eb701ea2016a067c9792e2308e5b
beyondpie <zsp07@tsinghua.org.cn>
2020-05-14 23:31:25 -0400

    minor.

--- model_v4/mc.sh
+++ model_v4/mc.sh
@@ -4,8 +4,8 @@
 
 for i in {1..4}
 do
-    ./${1} sample random seed=355113 \
+    ./${1} sample random seed=355113 num_samples=1000 num_warmup=1000\
          id=${i} data file=${2} \
-         output file=${1}${i}.csv >${1}${i}.log &
+         output refresh=5 file=${1}${i}.csv >${1}${i}.log &
 done
 
