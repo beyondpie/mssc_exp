8f654003ea64e00a1896f9d4b8eccf9551d587b1
beyondpie <zsp07@tsinghua.org.cn>
2020-05-27 11:43:33 -0400

    remove old models

--- model_v3/screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in 6 7
-do
-    model="v3_${i}"
-    make MODEL=${model} compile
-    make MODEL=${model} ssample >${model}_sample.log &
-done
-
--- model_v4/screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in 1
-do
-    model="v4-${i}"
-    make MODEL=${model} compile
-    make MODEL=${model} ssample >${model}_sample.log &
-done
-
