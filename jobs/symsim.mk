f00eb25d15e6d4eda9b085fe679e834c5b1ea41b
beyondpie <zsp07@tsinghua.org.cn>
2020-08-10 12:05:10 -0400

    Fix simudata_symsim in makefile

--- jobs/symsim.mk
+++ jobs/symsim.mk
@@ -1,13 +1,16 @@
 symsim := symsim
 symsim_tostan_script := ${pipline_dir}/3-1_symsim_to_bagwiff.R
 symsim_tostan_datadir := ${root}/${local_data_dir}/${symsim}/twostage_be_symsim/data
+symsim_tostan_pdfdir := ${root}/${local_data_dir}/${symsim}/twostage_be_symsim/pdf
 symsim_stan_outdir := ${root}/${exps}/${symsim}/stan
 symsimseed ?= 1
 symsim_gwise_data := ${symsim_tostan_datadir}/symsim_2be_${symsimseed}.rdump
 
 # * generate simulation data
 ${symsim_gwise_data}:
-	@Rscript ${symsim_tostan_script} --myseed ${symsimseed}
+	-mkdir -p ${symsim_tostan_datadir}
+	-mkdir -p ${symsim_tostan_pdfdir}
+	@Rscript ${symsim_tostan_script}
 
 .PHONY: simdata_symsim
 simdata_symsim: ${symsim_gwise_data}
