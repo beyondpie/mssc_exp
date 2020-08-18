7ac5f47d7d87336c84ae9635a6520e7df3a904ee
beyondpie <zsp07@tsinghua.org.cn>
2020-08-18 17:50:10 -0400

    Feat: add outliers detection

--- rutils/transform.R
+++ rutils/transform.R
@@ -258,3 +258,7 @@ calt <- function(delta, fn = colMeans) {
   names(sts) <- colnames(delta)
   return(sts)
 }
+
+is_outlier <- function(x) {
+  return(x < quantile(x, 0.25) - 1.5 * IQR(x) | x > quantile(x, 0.75) + 1.5 * IQR(x))
+}
