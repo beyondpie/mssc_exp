0ede885949065f7fe0921a30b57a6967c80943d6
beyondpie <zsp07@tsinghua.org.cn>
2020-09-01 00:28:21 -0400

    Add save stanfit sampling model.

--- src/modelcheck/fit_pbmc_cmdstanr.R
+++ src/modelcheck/fit_pbmc_cmdstanr.R
@@ -22,15 +22,12 @@ pbmccnt <- as.matrix(pbmcseurat@assays$RNA@counts)
 pbmcinds <- pbmcseurat@meta.data$patient
 pbmc_cellanno <- pbmcseurat@meta.data$seurat_clusters
 pbmc_cond <- pbmcseurat@meta.data$response
-
-## cytototic T cells
-mycluster <- c(2)
 ## ** select genes
 DEGs <- c("CCL4L1", "CCL4L2", "CCL3L1", "CCL3L3")
 heavyzeroGs <- c("MIR155HG", "TNFRSF4", "ICAM1", "NA.499", "HIST2H2AA4")
 heavyindeffectGs <- c("HBB", "HBA2", "HBA1")
-mygenes <- c(DEGs, heavyzeroGs, heavyindeffectGs)
 
+## * utilities
 get_stan_bagwiff_data <- function(whichgenes,
                                   whichcluster = mycluster,
                                   cnts,
@@ -49,16 +46,31 @@ get_stan_bagwiff_data <- function(whichgenes,
   mytotcnts <- colSums(cnts[, whichcells])[!outliers]
   myinds <- cellmeta_inds[whichcells][!outliers]
   myconds <- cellmeta_conds[whichcells][!outliers]
-  myt$to_bagwiff_r(mycnts, myinds, myconds, mytotcnts)
+  invisible(list(standata = myt$to_bagwiff_r(mycnts, myinds, myconds, mytotcnts),
+                 cnts = mycnts,
+                 totcnts = mytotcnts,
+                 inds <- myinds,
+                 conds <- myconds))
 }
 
-standata_cytoxictcell <- get_stan_bagwiff_data(mygenes, c(2),
-  pbmccnt,
-  pbmcinds,
-  pbmc_cond,
-  pbmc_cellanno, T)
 
-## * setup model
+get_init_nb_phi <- function(cnts) {
+  get_single_init <- function(x) {
+    tryCatch({
+      nbfit <- MASS::fitdistr(x, densfun = "negative binomial",
+                              lower = c(1e-5, 1e-5))
+      invisible(nbfit$estimate["size"])
+    }, error = function(cond) {
+      message(cond)
+      invisible(1.0)
+    }
+    )
+  }
+  vapply(seq_len(nrow(cnts)), FUN = function(i) {
+    get_single_init(cnts[i, ])
+  }, FUN.VALUE = 1.0)
+}
+
 get_mystanmodel <- function(fnm) {
   cmdstanr::cmdstan_model(
     stan_file = here("src", "stan", fnm),
@@ -68,31 +80,68 @@ get_mystanmodel <- function(fnm) {
   )
 }
 
-sampling_mystanmodel <- function(mystanmodel, mystandata, num_chains = 4) {
-  stanmodel_poiglm_gw_mc$sample(data = mystandata,
+sampling_mystanmodel <- function(mystanmodel,
+                                 mystandata,
+                                 mydelta = 0.82,
+                                 mytreedepth = 11,
+                                 num_chains = 4,
+                                 myinit = NULL) {
+  mystanmodel$sample(data = mystandata,
     seed = 355113L,
     refresh = 200,
     iter_warmup = 500,
     iter_sampling = 500,
-    adapt_delta = 0.85,
-    max_treedepth = 12,
+    adapt_delta = mydelta,
+    max_treedepth = mytreedepth,
     validate_csv = T,
     show_messages = T,
     chains = num_chains,
     parallel_chains = getOption("mc.cores", num_chains),
+    init = myinit,
     output_dir = here("src", "modelcheck",
       "stan_sampling")
   )
 }
 
+## ** model declare
 stanmodel_poiglm_gw_mc <- get_mystanmodel("v1-1.stan")
 stanmodel_nbglm_gw_mc <- get_mystanmodel("v3-1.stan")
-stanmodel_nblognm_gw_mc <- get_mystanmodel("v4-1.stan")
+
+## stanmodel_nblognm_gw_mc <- get_mystanmodel("v4-1.stan")
 
 ## * sampling
-num_chains <- 4
+## ** set data
+mygenes <- c("CCL3L3", "ICAM1", "HBB")
+## cytototic T cells
+mycluster <- c(2)
+standata_cytoxictcell <- get_stan_bagwiff_data(mygenes, mycluster,
+  pbmccnt,
+  pbmcinds,
+  pbmc_cond,
+  pbmc_cellanno, T)
+
+## ** sampling
 stanfit_poiglm_gw_mc <- sampling_mystanmodel(
-  stanmodel_poiglm_gw_mc, standata_cytoxictcell, num_chains)
+  stanmodel_poiglm_gw_mc, standata_cytoxictcell$standata,
+  0.83, 20, 4)
+stanfit_poiglm_gw_mc$save_object(file = here("src", "modelcheck",
+                                             "stan_save", "v1-1.rds"))
+
+## need to assign all the variables.
+## init_nb_phis <- get_init_nb_phi(standata_cytoxictcell$cnts)
+
+stanfit_nbglm_gw_mc <- sampling_mystanmodel(
+  stanmodel_nbglm_gw_mc, standata_cytoxictcell$standata,
+  0.85, 20, 4)
+stanfit_nbglm_gw_mc$save_object(file = here("src", "modelcheck",
+  "stan_save", "v3-1.rds"))
+
+## stanfit_nblognm_gw_mc <- sampling_mystanmodel(
+##   stanmodel_nblognm_gw_mc, standata_cytoxictcell,
+##   0.85, 20, 4
+## )
+
+## ** view results
 stanfit_poiglm_gw_mc$draws()
 
 ## csv_contents <- read_cmdstan_csv(fit$output_files())
