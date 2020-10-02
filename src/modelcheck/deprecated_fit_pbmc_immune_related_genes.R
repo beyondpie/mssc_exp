4386ff4d8e91f806ada7798600a5c2003ae0b46a
beyondpie <zsp07@tsinghua.org.cn>
2020-10-02 15:02:11 -0400

    minor, mark a unused file.

--- src/modelcheck/deprecated_fit_pbmc_immune_related_genes.R
+++ src/modelcheck/deprecated_fit_pbmc_immune_related_genes.R
@@ -1,3 +1,9 @@
+## Deprecated since the experiment in logic is not direct,
+## and hard to understand. e.g., what happened if the low-count
+## genes are themselves not immune-related.
+
+
+## Aim:
 ## use lower counts but differentially expressed genes from PBMC dataset.
 ## see if mssc can rank the genes related with immunne response higher.
 
@@ -5,6 +11,10 @@
 cmdstan_version <- "2.24.1" ## "2.23.0"
 my_cell_cluster <- c(2)
 pvalue_cutoff <- 0.05
+## roughly A is 1.5 times higher expressed than B or
+## A is 0.33 times lower expressed than B.
+## log2(1.5) ~ 0.58, 1/1.5 ~ 0.67
+log2foldchange_cutoff <- log2(1.5)
 
 ## load packages
 library(cmdstanr)
@@ -61,36 +71,24 @@ get_partial_pbmc_data <- function(pbmc_seurat,
 
 ## main
 ## * load PBMC data
-pbmc_datadir <- here("data", "antiPDL1_PBMC_IL8", "seurat.RDS")
-pbmc_seurat <- readRDS(pbmc_datadir)
+pbmc_seurat <- here("data", "antiPDL1_PBMC_IL8",
+                    "GSE145281_nicol.RDS") %>% readRDS(file = .)
 
 ## * select genes
 cytot_pbmc <- get_partial_pbmc_data(pbmc_seurat, my_cell_cluster)
 cytot_pseudobulk_deseq2 <- mypseudo$pseudobulk_deseq2(
-                                      cytot_pbmc$cnt,
-                                      cytot_pbmc$ind,
-                                      cytot_pbmc$cond)
+  cytot_pbmc$cnt,
+  cytot_pbmc$ind,
+  cytot_pbmc$cond)
 
-## ** use DESeq2 baseMean to select low-count genes.
+## ** use DESeq2 baseMean to select low-count/expressed genes.
+## use p-value instead of p-adj and log2foldchange to select the genes
+cytot_pseudobulk_degenes <- intersect(
+  which(cytot_pseudobulk_deseq2$pvalue <= pvalue_cutoff),
+  which(abs(cytot_pseudobulk_deseq2$log2FoldChange) >=
+    log2foldchange_cutoff))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## further filter high-expressed genes
+cytot_pseudobulk_deres <- cytot_pseudobulk_deseq2[
+  cytot_pseudobulk_degenes, ] %>%
+  .[.$baseMean <= quantile(.$baseMean, 0.25), ]
