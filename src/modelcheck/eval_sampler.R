7dab6518b6904984ea0ad4ccdb2f351f6f306bba
beyondpie <zsp07@tsinghua.org.cn>
2020-10-23 11:04:59 -0400

    add mu estimation.

--- src/modelcheck/eval_sampler.R
+++ src/modelcheck/eval_sampler.R
@@ -4,6 +4,8 @@
 library(cmdstanr)
 import::from(here, here)
 suppressPackageStartupMessages(library(tidyverse))
+library(bbmle)
+library(sads)
 
 ## local modules
 options("import.path" = here("rutils"))
@@ -20,7 +22,9 @@ options(mc.cores = 3)
 ## set cmdstan path
 set_cmdstan_path(path = paste(Sys.getenv("HOME"),
   "softwares",
-  "cmdstan-2.23.0", sep = "/"))
+  "cmdstan-2.23.0",
+  sep = "/"
+))
 
 ## * load pbmc for parameter estimate and setting.
 ## classical genes as DE
@@ -42,15 +46,19 @@ d1g <- "NFKB1"
 pbmc_seurat <- mypbmc$load_pbmc_seurat() %>%
   mypbmc$extract_from_seurat(pbmc_seurat = .)
 ## limit to the cell type
-subscdata <- mypbmc$get_celltype_specific_scdata(pbmc_seurat$cnt,
+subscdata <- mypbmc$get_celltype_specific_scdata(
+  pbmc_seurat$cnt,
   pbmc_seurat$resp,
   pbmc_seurat$inds,
   pbmc_seurat$ct,
-  cell_type)
+  cell_type
+)
 
 ## ** sample cell numbers
-sample_cells <- mypbmc$sample_cells_per_ind(subscdata$inds,
-                                            sample_cellnum)
+sample_cells <- mypbmc$sample_cells_per_ind(
+  subscdata$inds,
+  sample_cellnum
+)
 ## note individual order is changed according to sample_cells
 cnt <- subscdata$cnt[, sample_cells]
 inds <- subscdata$inds[sample_cells]
@@ -58,12 +66,58 @@ resp <- subscdata$resp[sample_cells]
 colsumcnt <- colSums(cnt)
 
 ## ** limit to one DE gene
-## should we remove outliers firstly?
 d1g_cnt <- cnt[d1g]
-par(mfrow=c(1,2))
-hist(d1g_cnt[resp==1])
-hist(d1g_cnt[resp==0])
+outliers <- myfit$is_outlier(d1g_cnt)
+d1g_cnt <- d1g_cnt[!outliers]
+
+par(mfrow = c(1, 2))
+hist(d1g_cnt[resp == 1])
+hist(d1g_cnt[resp == 0])
+
+## remove outliers
+d1g_inds <- inds[!outliers]
+d1g_resp <- resp[!outliers]
+d1g_sumcnt <- colsumcnt[!outliers]
 
 ## fit NB dist
+nb_fit_all <- myfit$prob_zero_nb(d1g_cnt, F)
+nb_fit_control <- myfit$prob_zero_nb(d1g_cnt[d1g_resp == 0], F)
+nb_fit_case <- myfit$prob_zero_nb(d1g_cnt[d1g_resp == 1], F)
+nb_fit_case_ind_mu <- vapply(paste0("R", seq_len(5)),
+  FUN = function(x) {
+    fit <- myfit$prob_zero_nb(d1g_cnt[d1g_inds == x])
+    invisible(log(fit$nbfit$estimate["mu"] / median(d1g_sumcnt[d1g_inds == x])))
+  }, FUN.VALUE = 0.0
+)
+nb_fit_control_ind_mu <- vapply(paste0("NR", seq_len(5)),
+  FUN = function(x) {
+    fit <- myfit$prob_zero_nb(d1g_cnt[d1g_inds == x])
+    invisible(log(fit$nbfit$estimate["mu"] / median(d1g_sumcnt[d1g_inds == x])))
+  }, FUN.VALUE = 0.0
+)
+
+
+## -------
+## fit NB for a list of genes, add see the distribution
+## of the dispersion.
+
+## -------
+
+## * assign initial values for the model
+mu0 <- log(nb_fit_all$nbfit$estimate["mu"] / median(d1g_sumcnt))
+mu_control <- log(nb_fit_control$nbfit$estimate["mu"] /
+  median(d1g_sumcnt[d1g_resp == 0])) - mu0
+mu_control_ind <- nb_fit_control_ind_mu -
+  log(nb_fit_control$nbfit$estimate["mu"] / median(d1g_sumcnt[d1g_resp == 0]))
+mu_case <- log(nb_fit_case$nbfit$estimate["mu"] /
+  median(d1g_sumcnt[d1g_resp == 1])) - mu0
+mu_case_ind <- nb_fit_case_ind_mu -
+  log(nb_fit_case$nbfit$estimate["mu"] / median(d1g_sumcnt[d1g_resp == 1]))
+
+
+
+
+
+
 
 
