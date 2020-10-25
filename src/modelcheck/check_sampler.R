81409856367094c620ca4f9d954b63a44f659400
beyondpie <zsp07@tsinghua.org.cn>
2020-10-24 23:01:19 -0400

    keep R instead of Rmd due to speed and convenience.

--- src/modelcheck/check_sampler.R
+++ src/modelcheck/check_sampler.R
@@ -1,19 +1,8 @@
----
-title: "Check gene-wise negative binomial model in MSSC"
-author: Songpeng Zu
-date: "`r format(Sys.time(), '%d %B %Y')`"
-urlcolor: blue
-fontsize: 12pt
-output:
-  html_document
----
-
-# Abstract
-Following Jun's suggestions, here we eval the sampler firstly by generating from the prior. 
+# Following Jun's suggestions, here we check
+# the sampler firstly by generating from the prior.
 
 # Set R environment
 
-```{r, cache = True}
 ## Following Jun's suggestions
 ## Eval the sampler firstly by generating from the prior.
 
@@ -42,22 +31,69 @@ options(warn = 0)
 options(mc.cores = 3)
 set.seed(355113)
 
-```
+# Setting functions
+get_fitted_mu <- function(y, vec_of_cond,
+                          vec_of_ind_under_cond,
+                          vec_of_s,
+                          num_of_ind_per_cond = 5,
+                          label_of_control = 0,
+                          nm_of_control = "NR",
+                          nm_of_case = "R") {
+  ## fit NB distribution, then
+  ## extract the different means for MSSC model.
 
-# load pbmc for parameter estimate and setting.
+  ## mean of individuals will be 5 control; 5 case in order.
+
+  y_fit <- myfit$prob_zero_nb(y, F)
+  mu0 <- log(y_fit$nbfit$estimate["mu"] / median(vec_of_s))
+
+  index_of_cond <- vec_of_cond == label_of_control
+  y_control_fit <- myfit$prob_zero_nb(y[index_of_cond], F)
+  mu_control <- log(y_control_fit$nbfit$estimate["mu"] /
+    median(vec_of_s[index_of_cond])) - mu0
+  mu_control_ind <- vapply(
+    paste0(nm_of_control, seq_len(num_of_ind_per_cond)),
+    FUN = function(x) {
+      index <- vec_of_ind_under_cond == x
+      fit <- myfit$prob_zero_nb(y[index], F)
+      invisible(log(fit$nbfit$estimate["mu"] /
+        median(vec_of_s[index])) - mu_control - mu0)
+    },
+    FUN.VALUE = 0.0
+  )
+
+  index_of_case <- vec_of_cond != label_of_control
+  y_case_fit <- myfit$prob_zero_nb(y[index_of_case], F)
+  mu_case <- log(y_case_fit$nbfit$estimate["mu"] /
+    median(vec_of_s[index_of_case])) - mu0
+  mu_case_ind <- vapply(
+    paste0(nm_of_case, seq_len(num_of_ind_per_cond)),
+    FUN = function(x) {
+      index <- vec_of_ind_under_cond == x
+      fit <- myfit$prob_zero_nb(y[index], F)
+      invisible(log(fit$nbfit$estimate["mu"] /
+        median(vec_of_s[index])) - mu_case - mu0)
+    },
+    FUN.VALUE = 0.0
+  )
+  invisible(list(mu0 = mu0,
+    mu_cond = c(mu_control, mu_case),
+    mu_ind = c(mu_control_ind, mu_case_ind)
+  ))
+}
+
+## * load pbmc for parameter estimate and setting.
 ## Classical genes as DE
-   - SNHG16, OASL, NAMPT, NFKB1, BCL2L11, TRAF4, ICAM1, XCL2, XCL1, CCL3L3, CCL3L1
-   
-## Strong individual effect genes 
-   - HBA1, HBA2, HBD
-   
+##   - SNHG16, OASL, NAMPT, NFKB1, BCL2L11, TRAF4, ICAM1, XCL2, XCL1, CCL3L3, CCL3L1
+## Strong individual effect genes
+##   - HBA1, HBA2, HBD
 ## possible non-DE
-   - TOX, YIPF5, CCL3, KDM6A, HDDC2
+##   - TOX, YIPF5, CCL3, KDM6A, HDDC2
 
-```{r cache = TRUE}
 cell_type <- "Naive CD4+ T"
-sample_cellnum <- 280
+num_of_cell_per_ind <- 280
 num_of_ind <- 10
+num_of_ind_per_cond <- 5
 num_of_cond <- 2
 # a DE gene: DE one gene
 d1g <- "NFKB1"
@@ -77,7 +113,7 @@ subscdata <- mypbmc$get_celltype_specific_scdata(
 ## ** sample cell numbers
 sample_cells <- mypbmc$sample_cells_per_ind(
   subscdata$inds,
-  sample_cellnum
+  num_of_cell_per_ind
 )
 ## note individual order is changed according to sample_cells
 cnt <- subscdata$cnt[, sample_cells]
@@ -121,11 +157,9 @@ nb_fit_control_ind_mu <- vapply(
   }, FUN.VALUE = 0.0
 )
 
-
 ## -------
 ## fit NB for a list of genes, add see the distribution
 ## of the dispersion.
-
 ## -------
 
 ## estimate the mu in the model.
@@ -139,22 +173,20 @@ mu_case <- log(nb_fit_case$nbfit$estimate["mu"] /
 mu_case_ind <- nb_fit_case_ind_mu -
   log(nb_fit_case$nbfit$estimate["mu"] / median(d1g_sumcnt[d1g_resp == 1]))
 
-```
 
 # set data for check model from prior
 
-```{r cache=TRUE}
 Ind <- c(vapply(seq_len(num_of_ind),
   FUN = function(x) {
-    rep(x, sample_cellnum)
+    rep(x, num_of_cell_per_ind)
   },
-  FUN.VALUE = c(rep(0.0, sample_cellnum))
+  FUN.VALUE = c(rep(0.0, num_of_cell_per_ind))
 ))
 Cond <- c(vapply(seq_len(num_of_cond),
   FUN = function(x) {
-    rep(x, sample_cellnum * num_of_ind / 2)
+    rep(x, num_of_cell_per_ind * num_of_ind / 2)
   },
-  FUN.VALUE = c(rep(0.0, sample_cellnum * num_of_ind / 2))
+  FUN.VALUE = c(rep(0.0, num_of_cell_per_ind * num_of_ind / 2))
 ))
 
 ## * set gwnb hyper prior
@@ -197,7 +229,7 @@ mucond <- rnorm(num_of_cond,
   mean = 0.0,
   sd = sqrt(tau2g)
 )
-y <- vapply(seq_len(sample_cellnum * num_of_ind),
+y <- vapply(seq_len(num_of_cell_per_ind * num_of_ind),
   FUN = function(i) {
     rnbinom(
       n = 1,
@@ -209,7 +241,7 @@ y <- vapply(seq_len(sample_cellnum * num_of_ind),
 )
 
 mydata <- list(
-  N = sample_cellnum * num_of_ind,
+  N = num_of_cell_per_ind * num_of_ind,
   K = num_of_ind,
   J = num_of_cond,
   S = d1g_sumcnt,
@@ -231,40 +263,33 @@ mydata <- list(
   alphaPhi2G = alphaPhi2G,
   betaPhi2G = betaPhi2G
 )
-```
 
 # Load and compile the stan model
-```{r cache = TRUE}
+vec_of_cond <- rep(1, )
 sbc_gwnb_model <- cmdstan_model(here(
   "src", "dirty_stan",
   "gwnb_simu_from_prior.stan"
 ), compile = T, force_recompile = T)
 
-```
 
 
 # analyze simulation-based calibration
 
+## setting the initial values
+vec_of_cond <- c(rep(1, num_of_cell_per_ind * num_of_ind_per_cond),
+  rep(2, num_of_cell_per_ind * num_of_ind_per_cond))
+vec_of_ind_under_cond <- c(paste0("NR",
+                                  rep(seq_len(num_of_ind_per_cond),
+                                      num_of_cell_per_ind)),
+  paste0("R", rep(seq_len(num_of_ind_per_cond), num_of_cell_per_ind)))
+
+sbc_mu_list <- get_fitted_mu(y, vec_of_cond, vec_of_ind_under_cond,
+  vec_of_s = d1g_sumcnt,
+  label_of_control = 1)
+
 ## analyze the variational inference
-```{r cache = TRUE}
-## sbc_gwnb_mle <- sbc_gwnb_model$optimize(
-##   data = mydata,
-##   seed = 1
-## )
 sbc_gwnb_vi_sampler <- sbc_gwnb_model$variational(
   data = mydata,
   seed = 1
 )
 sbc_gwnb_vi_draws <- sbc_gwnb_vi_sampler$draws()
-```
-
-## analyze the mcmc inference
-```{r eval=FALSE}
-sbc_gwnb_mcmc_sampler <- sbc_gwnb_model$sample(
-  data = mydata,
-  seed = 1
-  )
-
-sbc_gwnb_mcmc_draws <- sbc_gwnb_mcmc_sampler$draws()
-```
- 
