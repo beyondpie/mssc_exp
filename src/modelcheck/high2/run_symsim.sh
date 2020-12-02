a99d10db8b3dfe3ea7702768def5ba64e65b45dd
beyondpie <zsp07@tsinghua.org.cn>
2020-12-02 01:22:37 -0500

    add makefile and shell script to run symsim

---
+++ src/modelcheck/high2/run_symsim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+high2_symsim () {
+    Rscript high2_symsim.R --ratio_ind2cond $1 \
+	    --scale_in_diffg $2 \
+	    --scale_in_nondiffg $3 \
+	    --ngene $4 \
+	    --rpt $5 \
+	    --algorithm $6 \
+	    --eta $7 \
+	    --adapt_engaged $8 \
+	    --adapt_iter $9 \
+	    > high_ngene_${4}-ric_${1}-sid_${2}-sin_${3}-rpt_${5}-ag_${6}-eta_${7}-ae_${8}-ai_${9}.log 2>&1
+}
+
+
+"$@"
