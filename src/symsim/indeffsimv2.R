805b75768f945df7223e37d64758249b8dc79845
beyondpie <zsp07@tsinghua.org.cn>
2021-01-27 23:55:12 -0500

    add main in indeff

--- src/symsim/indeffsimv2.R
+++ src/symsim/indeffsimv2.R
@@ -33,9 +33,6 @@ mypseudo <- modules::import("pseudobulk")
 library(phytools)
 library(MASS)
 
-mysymsim <- modules::import("simulation_functions")
-## modules::reload(mysymsim)
-
 ## * help functions
 set_population_struct_using_phylo <- function(w = rep(0.1, 6)) {
   ## two populations (two conditiosn), while each has some subpopulations
@@ -123,7 +120,7 @@ get_symsim_umi <- function(phyla, bimod = 0, capt_alpha = 0.1,
   return(invisible(list(true = symsim_true, umi = symsim_umi)))
 }
 
-getDEgenes <- function(true_counts_res, popA, popB) {
+get_symsim_de_analysis <- function(true_counts_res, popA, popB) {
   meta_cell <- true_counts_res$cell_meta
   meta_gene <- true_counts_res$gene_effects
   ## when popA or popB has multiple subpopulations
@@ -147,27 +144,19 @@ getDEgenes <- function(true_counts_res, popA, popB) {
     return(log2(mean(s_mat[igene, popA_idx] * kon_mat[igene, popA_idx] / (kon_mat[igene, popA_idx] + koff_mat[igene, popA_idx])) /
       mean(s_mat[igene, popB_idx] * kon_mat[igene, popB_idx] / (kon_mat[igene, popB_idx] + koff_mat[igene, popB_idx])))))
 
-  true_counts <- true_counts_res$counts
-  true_counts_norm <- t(t(true_counts) / colSums(true_counts)) * 10^6
-
-  ## wil.p_true_counts <- sapply(1:ngenes, function(igene)
-  ##   return(wilcox.test(true_counts_norm[igene, popA_idx], true_counts_norm[igene, popB_idx])$p.value))
-
-  ## wil.adjp_true_counts <- p.adjust(wil.p_true_counts, method = 'fdr')
-
   return(list(nDiffEVF = n_useDEevf, logFC_theoretical = logFC_theoretical))
 }
 
 set_mssc_meta <- function(symsim, phyla, logfc_threshold = 0.6) {
   ## dea: DE gene analysis results
-  dea <- getDEgenes(symsim$true, popA = phyla$pop$sub1, popB = phyla$pop$sub2)
+  dea <- get_symsim_de_analysis(symsim$true, popA = phyla$pop$sub1, popB = phyla$pop$sub2)
 
   ## TODO: set several criterias
   diffg <- which((dea$logFC_theoretical >= logfc_threshold) & (dea$nDiffEVF > 0))
   nondiffg <- setdiff(seq_len(length(dea$logFC_theoretical)), diffg)
 
   ind <- symsim$umi$cell_meta$pop
-  
+
   ncell <- length(ind)
   cond <- floor(rep(0, ncell))
   cond[ind %in% phyla$pop$sub1] <- 1
@@ -178,14 +167,13 @@ set_mssc_meta <- function(symsim, phyla, logfc_threshold = 0.6) {
   cond_of_ind[phyla$pop$sub1] <- 1
   cond_of_ind[phyla$pop$sub2] <- 2
   return(invisible(list(dea = dea,
-                        diffg = diffg,
-                        nondiffg = nondiffg,
-                        logfc_threshold = logfc_threshold,
-                        ind = ind,
-                        cond = cond,
-                        cond_of_ind = cond_of_ind)))
+    diffg = diffg,
+    nondiffg = nondiffg,
+    logfc_threshold = logfc_threshold,
+    ind = ind,
+    cond = cond,
+    cond_of_ind = cond_of_ind)))
 }
-
 
 ## plot utilities
 plot_tree <- function(p) {
@@ -213,10 +201,10 @@ plot_tsne <- function(symsim_umi, color_values) {
   return(invisible(p))
 }
 
-plotviolin <- function(cnt, ind, genes, logtpm = F,
-                       myggtitle = theme(
-                         plot.title = element_text(
-                           size = 15, hjust = 0.5))) {
+violin_plot <- function(cnt, ind, genes, logtpm = F,
+                        myggtitle = theme(
+                          plot.title = element_text(
+                            size = 15, hjust = 0.5))) {
   n <- length(genes)
   gnm <- paste0("gene", genes)
   select_cnt <- cnt[genes, ]
@@ -247,22 +235,22 @@ plotviolin <- function(cnt, ind, genes, logtpm = F,
   return(plist)
 }
 
-plot_genes_after_batcheffect <- function(symsim_umi,
-                                         ind,
-                                         diffg,
-                                         nondiffg,
-                                         nde = 40, nnde = 40,
-                                         pnrow = 5,
-                                         save_path = NULL,
-                                         save_figure = TRUE,
-                                         fnm_prefix = "symsim_violin",
-                                         width = 20, height = 10) {
+plot_de_violin <- function(symsim_umi,
+                           ind,
+                           diffg,
+                           nondiffg,
+                           nde = 40, nnde = 40,
+                           pnrow = 5,
+                           save_path = NULL,
+                           save_figure = TRUE,
+                           fnm_prefix = "symsim_violin",
+                           width = 20, height = 10) {
   ## show batch effect using violin plot for different genes.
   ## nde: num of differential genes to show
   ## nnde: num of non-differential genes to show
 
   ## violinplot for diffg
-  pviolin_symsimdeg <- plotviolin(symsim_umi$counts, ind, diffg)
+  pviolin_symsimdeg <- violin_plot(symsim_umi$counts, ind, diffg)
   n <- ifelse(nde > length(diffg), length(diffg), nde)
   sampled_pvd <- ggarrange(
     plotlist = pviolin_symsimdeg[sample(length(diffg), n, replace = F)],
@@ -271,7 +259,7 @@ plot_genes_after_batcheffect <- function(symsim_umi,
   )
 
   ## violinplot for nondiffg
-  pviolin_symsim_sndeg <- plotviolin(symsim_umi$counts, ind, nondiffg)
+  pviolin_symsim_sndeg <- violin_plot(symsim_umi$counts, ind, nondiffg)
   m <- ifelse(nnde > length(nondiffg), length(nondiffg), nnde)
   sampled_pvsnd <- ggarrange(
     plotlist = pviolin_symsim_sndeg[sample(length(nondiffg), m, replace = F)],
@@ -296,6 +284,65 @@ plot_genes_after_batcheffect <- function(symsim_umi,
   ))
 }
 
+simu <- function(ncell_per_ind = 30, nind_per_cond = 3, brn_len = 0.5,
+                 bimod = 0, sigma = 0.6, capt_alpha = 0.2, ngene = 200,
+                 seed = 1L) {
+  ## simulate individual effect using symsim
+  ncond <- 2
+  nind <- nind_per_cond * ncond
+  ncell <- nind * ncell_per_ind
+  w <- rep(brn_len, nind)
+  phyla <- set_population_struct_using_phylo(w = brn)
+  symsim <- get_symsim_umi(phyla = phyla, bimod = bimod,
+    capt_alpha = capt_alpha, gene_module_prop = 0.0,
+    ncell_per_subpop = ncell_per_ind,
+    sigma = sigma,
+    ngene = ngene,
+    seed = seed,
+    nevf = 10,
+    n_de_evf = 7,
+    vary = "s")
+  dea <- get_symsim_de_analysis(true_counts_res = symsim$true,
+    popA = phyla$pop$sub1,
+    popB = phyla$pop$sub2)
+  return(invisible(list(phyla = phyla,
+    symsim = symsim,
+    dea = dea)))
+}
+
+load_mssc <- function(nind = 10, mssc_version = "mssc_2-0",
+                      tol_rel_obj = 1e-06,
+                      num_iter = 20000, output_samples = 3000) {
+  ## nind: total number individuals
+  ## tol_rel_obj, num_iter, output_samples are used
+  ## to tune the model
+  ## return:
+  ## - a mssc model
+  mssc_path <- here::here("src", "mssc")
+  model <- modules::import_(mssc_version)
+  invisible(model$High2$new(
+    stan_snb_path = file.path(mssc_path, "stan", "snb.stan"),
+    stan_high2_path = file.path(mssc_path, "stan",
+                                paste0(mssc_version, ".stan")),
+    nind = nind,
+    tol_rel_obj = tol_rel_obj,
+    algorithm = "meanfield",
+    adapt_engaged = FALSE,
+    eta = 0.1,
+    ## or iter more steps
+    num_iter = num_iter,
+    output_samples = output_samples
+  ))
+}
+
+set_result_array <- function(rpt = 5, ncells = c(20, 40, 80),
+                             methods = c("mssc_2-0", "pseudo", "wilcox", "t")) {
+  ## setup the result array:
+  ## - num_measurement by length(ncell) by repeat_num
+  invisible(array(data = NA, dim = c(length(methods), length(ncells), rpt),
+                  dimnames = list(methods, ncells, seq_len(rpt))))
+}
+
 ## * main
 phyla3 <- set_population_struct_using_phylo(w = c(0.2, 0.5, 0.4, 0.4, 0.5, 0.3))
 plot_tree(phyla3)
@@ -308,27 +355,98 @@ p_tsne
 
 ## de analysis
 mssc_meta <- set_mssc_meta(symsim = symsim, phyla = phyla3)
-p <- plot_genes_after_batcheffect(symsim_umi = symsim$umi,
-                                  ind = mssc_meta$ind,
-                                  diffg = mssc_meta$diffg,
-                                  nondiffg = mssc_meta$nondiffg,
-                                  nde = 20, nnde = 20,
-                                  save_figure = F)
+p <- plot_de_violin(symsim_umi = symsim$umi,
+  ind = mssc_meta$ind,
+  diffg = mssc_meta$diffg,
+  nondiffg = mssc_meta$nondiffg,
+  nde = 20, nnde = 20,
+  save_figure = F)
 p$spvln_degs
 p$spvln_ndegs
 
-## violin
-genes <- sample(seq_len(100), size = 50, replace = F)
-p_violins <- plotviolin(cnt = symsim$umi$counts, ind = symsim$umi$cell_meta$pop,
-  genes = genes)
-p <- ggarrange(plotlist = p_violins, nrow = 10, ncol = 5)
-p
+main <- function(nind_per_cond,
+                 brn_len,
+                 bimod,
+                 sigma,
+                 ncells,
+                 capt_alpha,
+                 ngene = 100,
+                 rpt = 5,
+                 save_figure = TRUE,
+                 fig_width = 20,
+                 fig_height = 10,
+                 save_symsim_data = FALSE,
+                 save_mssc_model = FALSE) {
+  ## argument:
+  ## - ncells: vector of ncell_per_ind
+  
+  ## set local path to save results
+  result_dir <- here::here("src", "symsim",
+                           paste0("symsim_", format(Sys.time(), format = "%Y%m%d%H%M")))
+  simu_fig_dir <- file.path(result_dir, "simu_figs")
+  simu_data_dir <- file.path(result_dir, "simu_data")
+  dea_dir <- file.path(result_dir, "dea")
+  mssc_v2_dir <- file.path(result_dir, "mssc_v2")
 
+  for ( d in c(result_dir, simu_fig_dir, simu_data_dir, dea_dir, mssc_v2_dir)) {
+    if (!dir.exists(d)) {
+      dir.create(path = d)
+    }
+  }
+  
+  ## load mssc model
+  nind_all <- nind_per_cond * 2
+  mssc_20 <- load_mssc(nind = nind_all, mssc_version = "mssc_2-0")
 
+  ## declare the result
+  r <- set_result_array(rpt = rpt, ncells = ncells,
+                        methods = c("mssc_2-0", "pseudo", "wilcox", "t"))
 
-## logtpm
-## p_violins <- plotviolin(cnt = symsim$umi$counts, ind = symsim$umi$cell_meta$pop,
-##                         logtpm = T,
-##            genes = genes)
-## p <- ggarrange(plotlist = p_violins, nrow = 10, ncol = 5)
-## p
+  for (i in seq_len(rpt)) {
+    r_i <- matrix(NA, nrow = dim(r)[1], ncol = dim(r)[2])
+    rownames(r_i) <- rownames(r)
+    for (j in seq_along(ncells)) {
+      ncell <- ncells[j]
+      ## simulate data
+      symsim_data_fnm <- str_glue("{ngene}gene", "{nind_all}ind",
+                                  "{ncell}cell", "{brn_len}w", "{bimod}bimod",
+                                  "{sigma}sigma", "{capt_alpha}alpha", "{i}seed",
+                                  "{rpt}rpt.rds", .sep = "_")
+      mysimu <- simu(ncell_per_ind = ncell,
+                     nind_per_cond = nind_per_cond,
+                     brn_len = brn_len,
+                     bimod = bimod,
+                     sigma = sigma,
+                     capt_alpha = capt_alpha,
+                     ngene = ngene,
+                     seed = i)
+    } ## end of ncells
+  } ## end of rpt
+  
+}
+
+library(optparse)
+option_list <- list(
+  ## make_option(c("--ncell_per_ind"), action = "store", type = "integer", default = 30),
+  ## make_option(c("--ngene"), action = "store", type = "integer", default = 100),
+  make_option(c("--nind_per_cond"), action = "store", type = "integer", default = 3),
+  make_option(c("--brn_len"), action = "store", type = "double", default = 0.5),
+  make_option(c("--bimod"), action = "store", type = "double", default = 0.1),
+  make_option(c("--sigma"), action = "store", type = "double", default = 0.6),
+  make_option(c("--capt_alpha"), action = "store", type = "double", default = 0.2)
+)
+
+args <- parser_args(OptionParser(option_list = option_list))
+main(
+  nind_per_cond = args$nind_per_cond,
+  brn_len = args$brn_len,
+  bimod = args$bimod,
+  sigma = args$sigma,
+  ncells = c(30, 50, 80, 120, 160, 240, 300),
+  rpt = 5,
+  save_figure = T,
+  fid_width = 20,
+  fig_height = 10,
+  save_symsim_data = FALSE,
+  save_mssc_model = FALSE
+)
