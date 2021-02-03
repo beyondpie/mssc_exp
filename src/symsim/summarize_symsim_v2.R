2f58018f2cb26b80f5429d014d8f91896762e341
beyondpie <zsp07@tsinghua.org.cn>
2021-02-02 22:27:22 -0500

    add summary code of indeffv2

---
+++ src/symsim/summarize_symsim_v2.R
@@ -0,0 +1,11 @@
+## summarize results from symsim v2
+library(tidyverse)
+library(ggplot2)
+library(grid)
+library(gtable)
+library(gridExtra)
+library(ggpubr)
+
+## Result
+## - mssc_symsimv2_5subpop_1.0brnlen_1bimod-_0.4sigma_0.1alpha_100gene.log
+## - mssc_symsimv2_5subpop_0.5brnlen_0bimod-_0.4sigma_0.2alpha_100gene.log
