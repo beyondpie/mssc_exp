6232d4e3862237b682220eec3c76c3f6383a6ddf
beyondpie <szu@health.ucsd.edu>
2024-06-05 22:19:04 -0700

    split based on major type and then covert to seurat

--- 01.data.preprocess/script/01.covid19_large_cohort.py
+++ 01.data.preprocess/script/01.covid19_large_cohort.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from subprocess import Popen
 from scipy import sparse
 import numpy as np
 import pandas as pd
@@ -60,13 +61,42 @@ cvd.write_h5ad(filename = os.path.join(proj_root, "data", "COVID19_large_cohort"
                                        "covid19.large.cohort.ann.h5ad"))
 
 # * to Seurat, organized by major types
-R: str = os.path.join("/home/szu/miniforge3/envs/r/bin/Rscript")
-scipyann2seurat_script = os.path.join
+projd = pyprojroot.here()
+R: str = "/home/szu/miniforge3/envs/r/bin/Rscript"
+scanpyann2seurat_script = os.path.join(projd, "tools", "singlecell",
+                                      "ScanpyAnnData2Seurat.R")
+covid19_cohort_dir = os.path.join(projd,
+                                  "data", "COVID19_large_cohort")
+covid19_mt_dir = os.path.join(covid19_cohort_dir, "majortype")
+os.makedirs(covid19_mt_dir, exist_ok = True)
+conda = "/home/szu/miniforge3/bin/conda"
+
+
 ann = ad.read_h5ad(
-    filename = os.path.join(
-        proj_root, "data", "COVID19_large_cohort",
+    filename = os.path.join(covid19_cohort_dir,
         "covid19.large.cohort.ann.h5ad")
 )
-
 majorTypes = ann.obs.majorType.unique().to_list()
+
+def subset_by_mt(mt:str) -> ad.AnnData:
+    r: ad.AnnData = ann[ann.obs.majorType == mt].copy()
+    r.write_h5ad(filename = os.path.join(covid19_mt_dir,
+                                         f"covid19.large.{mt}.h5ad"))
+    return(r)
+for mt in majorTypes:
+    print(mt)
+    subset_by_mt(mt)
+
+def get_ann2seurat_exp(mt: str) -> str:
+    annfnm = os.path.join(covid19_mt_dir, f"covid19.large.{mt}.h5ad")
+    outfnm = os.path.join(covid19_mt_dir, f"covid19.large.{mt}.seu.rds")
+    r = [R, scanpyann2seurat_script, f"-f {annfnm}",
+         f"-o {outfnm}", f"--conda {conda}", f"--condaenv sa2"]
+    return ' '.join(r)
+    
+for mt in majorTypes:
+    print(mt)
+    cmd = get_ann2seurat_exp(mt = mt)
+    p = Popen(cmd, shell = True)
+    p.wait()
 
